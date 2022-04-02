3c36b1ba5857bf808ed5c187c526a79cfdd7cd2b
h.itagaki <mint.daa.a2@gmail.com>
2022-04-02 23:00:07 +0900

    phpコンテナ追加

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -1,28 +1,11 @@
-resource "aws_ecr_repository" "nginx" {
+module "nginx" {
+  source = "../../../../modules/ecr"
+
   name = "example-prod-foobar-nginx"
-  tags = {
-    Name = "example-prod-foobar-nginx"
-  }
 }
 
-resource "aws_ecr_lifecycle_policy" "nginx" {
-  policy = jsonencode(
-    {
-      "rules" : [
-        {
-          "rulePriority" : 1,
-          "description" : "Hold only 10 images, expire all others",
-          "selection" : {
-            "tagStatus" : "any",
-            "countType" : "imageCountMoreThan",
-            "countNumber" : 10
-          },
-          "action" : {
-            "type" : "expire"
-          }
-        }
-      ]
-    }
-  )
-  repository = aws_ecr_repository.nginx.name
+module "php" {
+  source = "../../../../modules/ecr"
+
+  name = "example-prod-foobar-php"
 }
